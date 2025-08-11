6a58734ad9749fcda3ed750e1a8f14347f195719
RendonnM <srendonm@correo.iue.edu.co>
2025-08-10 19:59:12 -0500

    feat(recursion)📝: Add Fibonacci recursion example from Grokking Algorithms

--- src/books/Grokking Algorithms/3. Recursion/recursion.ts
+++ src/books/Grokking Algorithms/3. Recursion/recursion.ts
@@ -4,3 +4,11 @@ const factorial = (n: number): number => {
 }
 
 console.log(`The factorial of 3 is ${factorial(3)}`)
+
+
+function fibonacci(n: number): number {
+  if (n <= 1) return n
+  return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+console.log(`The Fibonnaci number at position 5 is ${fibonacci(5)}`)

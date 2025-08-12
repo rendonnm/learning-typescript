83c92f77f3909505feb66a7735dd6bd5d4f4719e
RendonnM <srendonm@correo.iue.edu.co>
2025-08-11 22:34:48 -0500

    feat(exercises)✨: Add exercise 4.3 and notes from the 4th chapter of Grokking Algorithms

--- src/books/Grokking Algorithms/4. Quicksort/exercises.ts
+++ src/books/Grokking Algorithms/4. Quicksort/exercises.ts
@@ -1,20 +1,41 @@
-// 4. Write out the code for the earlier sum function
-const sum = (arr: number[]): number => {
-  if (arr.length === 1) return arr[0]
-  return arr[0] + sum(arr.slice(1))
-}
-console.log(sum([2, 4, 6]))
+type NumbersArray = number[]
+type Index = number
 
-// const sumRecursivelly = (arr: number[], idx: number = 0): number => {
-//   if (idx >= arr.length) return 0
-//   return arr[idx] + sumRecursivelly(arr, idx + 1)
+// 4. Write out the code for the earlier sum function
+// const sum = (arr: NumbersArray): number => {
+//   if (arr.length === 1) return arr[0]
+//   return arr[0] + sum(arr.slice(1))
 // }
-// console.log(sumRecursivelly([2, 4, 6]))
+// console.log(sum([2, 4, 6]))
+
+const sumRecursivelly = (arr: NumbersArray, idx: Index = 0): number => {
+  if (idx >= arr.length) return 0
+  return arr[idx] + sumRecursivelly(arr, idx + 1)
+}
+console.log(sumRecursivelly([2, 4, 6]))
 
 // 4.2 Write a recursive function to count the nummber of items in a list
-const countRecursivelly = (arr: number[]): number => {
-  if (arr.length === 1) return 1
-  return 1 + countRecursivelly(arr.slice(1))
+// const countRecursivelly = (arr: NumbersArray): number => {
+//   if (arr.length === 1) return 1
+//   return 1 + countRecursivelly(arr.slice(1))
+// }
+
+// console.log(countRecursivelly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+const countRecursivelly = (arr: NumbersArray, idx: Index = 0): number => {
+  if (arr[idx] === undefined) return 0
+  return 1 + countRecursivelly(arr, idx + 1)
 }
 
 console.log(countRecursivelly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+
+// 4.3 Write a recursive function to find the maximum number in a list.
+function maxNumberRecursive(arr: NumbersArray, idx: Index = 0, maxNumber = -Infinity): number {
+  if (arr[idx] === undefined) return maxNumber
+  if (arr[idx] > maxNumber) {
+    maxNumber = arr[idx]
+  }
+  return maxNumberRecursive(arr, ++idx, maxNumber)
+}
+
+console.log(maxNumberRecursive([2, 45, 18, 392, 3, 1, 34, 98, 201, 5]))
--- src/books/Grokking Algorithms/4. Quicksort/notes.md
+++ src/books/Grokking Algorithms/4. Quicksort/notes.md
@@ -2,8 +2,18 @@
 
 ## Notes
 
+### Steps for D&C
+
+1. Figure out the base case (simplest posible case)
+2. Divide or decrease the problem until it becomes the base case
+
+### Tip
+When we are working with arrays the base case is often an empty array or an array with one element.
+
 ## Exercises
 
 ### 4.1 Write out the code for the earlier sum function.
 
 ### 4.2 Write a recursive function to count the number of items in a list
+
+### 4.3 Write a recursive function to find the maximum number in a list.

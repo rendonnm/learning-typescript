70f25cdaf1f3dcc0c92a144423debadaabf4075f
RendonnM <srendonm@correo.iue.edu.co>
2025-08-12 22:32:43 -0500

    feat(exercises)✨: Add exercise 4.4 from the 4th chapter of Grokking Algorithms

--- src/books/Grokking Algorithms/4. Quicksort/exercises.ts
+++ src/books/Grokking Algorithms/4. Quicksort/exercises.ts
@@ -12,7 +12,7 @@ const sumRecursivelly = (arr: NumbersArray, idx: Index = 0): number => {
   if (idx >= arr.length) return 0
   return arr[idx] + sumRecursivelly(arr, idx + 1)
 }
-console.log(sumRecursivelly([2, 4, 6]))
+console.log(sumRecursivelly([2, 4, 6])) // 12
 
 // 4.2 Write a recursive function to count the nummber of items in a list
 // const countRecursivelly = (arr: NumbersArray): number => {
@@ -27,7 +27,7 @@ const countRecursivelly = (arr: NumbersArray, idx: Index = 0): number => {
   return 1 + countRecursivelly(arr, idx + 1)
 }
 
-console.log(countRecursivelly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+console.log(countRecursivelly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // 10
 
 // 4.3 Write a recursive function to find the maximum number in a list.
 function maxNumberRecursive(arr: NumbersArray, idx: Index = 0, maxNumber = -Infinity): number {
@@ -38,4 +38,21 @@ function maxNumberRecursive(arr: NumbersArray, idx: Index = 0, maxNumber = -Infi
   return maxNumberRecursive(arr, idx + 1, maxNumber)
 }
 
-console.log(maxNumberRecursive([2, 45, 18, 392, 3, 1, 34, 98, 201, 5]))
+console.log(maxNumberRecursive([2, 45, 18, 392, 3, 1, 34, 98, 201, 5])) // 392
+
+//4.3 Remember binary search from chapter 1? It’s a D&C algorithm, too. 
+// Can you come up with the base case and recursive case for binary search?
+function recursiveBinarySearch(arr: NumbersArray, target: number, left: number = 0, right: number = arr.length - 1) {
+  if (left > right) return null
+
+  const middlePoint = Math.floor((left + right) / 2)
+  if (arr[middlePoint] === target) return middlePoint
+
+  if (arr[middlePoint] > target) {
+    return recursiveBinarySearch(arr, target, left, middlePoint - 1)
+  } else {
+    return recursiveBinarySearch(arr, target, middlePoint + 1, right)
+  }
+}
+
+console.log(recursiveBinarySearch([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], 40)) //3

4f0292fe3403ec2ad0678048950f527decd161c4
RendonnM <srendonm@correo.iue.edu.co>
2025-08-25 21:57:34 -0500

    feat(exercises)✨: Add exercises 4.5 - 4.8 from the 4th chapter of Grokking Algorithms

--- src/books/Grokking Algorithms/4. Quicksort/exercises.ts
+++ src/books/Grokking Algorithms/4. Quicksort/exercises.ts
@@ -1,5 +1,5 @@
-type NumbersArray = number[]
-type Index = number
+type NumbersArray = number[];
+type Index = number;
 
 // 4.1 Write out the code for the earlier sum function
 // const sum = (arr: NumbersArray): number => {
@@ -9,10 +9,10 @@ type Index = number
 // console.log(sum([2, 4, 6]))
 
 const sumRecursivelly = (arr: NumbersArray, idx: Index = 0): number => {
-  if (idx >= arr.length) return 0
-  return arr[idx] + sumRecursivelly(arr, idx + 1)
-}
-console.log(sumRecursivelly([2, 4, 6])) // 12
+	if (idx >= arr.length) return 0;
+	return arr[idx] + sumRecursivelly(arr, idx + 1);
+};
+console.log(sumRecursivelly([2, 4, 6])); // 12
 
 // 4.2 Write a recursive function to count the nummber of items in a list
 // const countRecursivelly = (arr: NumbersArray): number => {
@@ -23,36 +23,56 @@ console.log(sumRecursivelly([2, 4, 6])) // 12
 // console.log(countRecursivelly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
 const countRecursivelly = (arr: NumbersArray, idx: Index = 0): number => {
-  if (arr[idx] === undefined) return 0
-  return 1 + countRecursivelly(arr, idx + 1)
-}
+	if (arr[idx] === undefined) return 0;
+	return 1 + countRecursivelly(arr, idx + 1);
+};
 
-console.log(countRecursivelly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // 10
+console.log(countRecursivelly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 10
 
 // 4.3 Write a recursive function to find the maximum number in a list.
-function maxNumberRecursive(arr: NumbersArray, idx: Index = 0, maxNumber = -Infinity): number {
-  if (arr[idx] === undefined) return maxNumber
-  if (arr[idx] > maxNumber) {
-    maxNumber = arr[idx]
-  }
-  return maxNumberRecursive(arr, idx + 1, maxNumber)
+function maxNumberRecursive(
+	arr: NumbersArray,
+	idx: Index = 0,
+	maxNumber = -Infinity,
+): number {
+	if (arr[idx] === undefined) return maxNumber;
+	if (arr[idx] > maxNumber) {
+		maxNumber = arr[idx];
+	}
+	return maxNumberRecursive(arr, idx + 1, maxNumber);
 }
 
-console.log(maxNumberRecursive([2, 45, 18, 392, 3, 1, 34, 98, 201, 5])) // 392
+console.log(maxNumberRecursive([2, 45, 18, 392, 3, 1, 34, 98, 201, 5])); // 392
 
-//4.4 Remember binary search from chapter 1? It’s a D&C algorithm, too. 
+//4.4 Remember binary search from chapter 1? It’s a D&C algorithm, too.
 // Can you come up with the base case and recursive case for binary search?
-function recursiveBinarySearch(arr: NumbersArray, target: number, left: number = 0, right: number = arr.length - 1) {
-  if (left > right) return null
+function recursiveBinarySearch(
+	arr: NumbersArray,
+	target: number,
+	left: number = 0,
+	right: number = arr.length - 1,
+) {
+	if (left > right) return null;
 
-  const middlePoint = Math.floor((left + right) / 2)
-  if (arr[middlePoint] === target) return middlePoint
+	const middlePoint = Math.floor((left + right) / 2);
+	if (arr[middlePoint] === target) return middlePoint;
 
-  if (arr[middlePoint] > target) {
-    return recursiveBinarySearch(arr, target, left, middlePoint - 1)
-  } else {
-    return recursiveBinarySearch(arr, target, middlePoint + 1, right)
-  }
+	if (arr[middlePoint] > target) {
+		return recursiveBinarySearch(arr, target, left, middlePoint - 1);
+	} else {
+		return recursiveBinarySearch(arr, target, middlePoint + 1, right);
+	}
 }
 
-console.log(recursiveBinarySearch([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], 40)) //3
+console.log(
+	recursiveBinarySearch([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], 40),
+); //3
+
+// 4.5 Printing the value of each element in an array
+// R.) O(1) * O(n) = O(n)
+// 4.6 Doubling the value of each element in an array
+// R.) O(1) * O(n) = O(n)
+// 4.7 Doubling the value of just the first element in an array
+// R.) O(1) * O(1) = O(1)
+// 4.8 Creating a multiplication table with all the elements in the array. So if your array is [2, 3, 7, 8, 10], you first multiply every element by 2, then multiply every element by 3, then by 7, and so on.
+// R.) O(n) * O(n) = O(n^2)
